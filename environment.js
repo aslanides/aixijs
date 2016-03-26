b8b8ff4b6860c96a10c82c0f4a15f9a5784577f3
John Stewart Aslanides <john.aslanides@anu.edu.au>
2016-03-27 00:12:38 +1100

    move methods into Gridworld class

--- environment.js
+++ environment.js
@@ -1,12 +1,12 @@
 class Environment {
     constructor() {
-        this.initial_state = 0
         this.actions = []
-        this.num_states = 0
         this.reward = 0
         this.optimal_average_reward = 0
     }
-    perform(action) {}
+    perform(action) {
+        throw "Not implemented!"
+    }
 }
 
 class Gridworld extends Environment {
@@ -23,86 +23,67 @@ class Gridworld extends Environment {
             }
         }
         this.num_states = this.M * this.N
-    }
-}
-
-class EpisodicGrid extends Gridworld {
-    constructor(map) {
-        super(map)
-        this.episodic = typeof episodic === 'boolean' ? episodic : true
-        this.pos = {
-            x : 0,
-            y : 0
-        }
-        this.chocolate = 10;
-        this.initial_state = this.encode_percept()
-        this.optimal_average_reward = this.chocolate / 26 // magic number 26 is how many steps to the choclate from beginning
         this.actions = [
             function(e) {return e.move(-1,0)},
             function(e) {return e.move(1,0)},
             function(e) {return e.move(0,1)},
             function(e) {return e.move(0,-1)}
         ]
-    }
-    consume_chocolate(x,y) {
-        /*
-        this.tiles[x][y].type = "F"
-        var i; var j;
-        while (true) {
-            i = Math.floor(Math.random() * this.M)
-            j = Math.floor(Math.random() * this.N)
-            if (this.tiles[i][j].type == "F") {
-                this.tiles[i][j].type = "C"
-                break
-            }
+        this.pos = {
+            x : 0,
+            y : 0
         }
-        */
-        this.pos.x = 0
-        this.pos.y = 0
+        this.initial_state = this.encode_percept()
     }
-
-    move(xx,yy) {
-        this.pos.x = this.pos.x+xx;
-        this.pos.y = this.pos.y+yy;
-        if (!this.check_move()) {
-            this.pos.x = this.pos.x-xx;
-            this.pos.y = this.pos.y-yy;
-        }
-        return this.encode_percept()
-    }
-
     encode_percept() {
         return {
             obs: this.M * this.pos.x + this.pos.y,
             rew : this.reward
         }
     }
-
     perform(action) {
         return this.actions[action](this)
     }
+    move(xx,yy) {
+        var newx = this.pos.x + xx
+        var newy = this.pos.y + yy
+        if (this.legal(newx,newy)) {
+            this.pos.x = newx
+            this.pos.y = newy
+        }
+        return this.encode_percept()
+    }
+    inbounds(x,y) {
+        return !(x < 0 || y < 0 || x >= this.M || y >= this.N)
+    }
+    legal(x,y) {
+        throw "Not implemented!"
+    }
+}
 
-    check_move() {
-        if (this.pos.x < 0 || this.pos.y < 0 || this.pos.x >= this.M || this.pos.y >= this.N) {
+class EpisodicGrid extends Gridworld {
+    constructor(map) {
+        super(map)
+        this.chocolate = 10;
+        this.optimal_average_reward = this.chocolate / 26 // magic number 26 is how many steps to the chocolate from beginning
+    }
+
+    legal(newx,newy) {
+        if (!this.inbounds(newx,newy)) {
+            this.reward = -1
+            return false
+        }
+        var type = this.tiles[newx][newy].type
+        if (type == "W") {
             this.reward = -1
             return false;
-        } else {
-            var type = this.tiles[this.pos.x][this.pos.y].type
-            if (type == "W") {
-                this.reward = -1
-                return false;
-            } else if (type == "C"){
-                this.reward = this.chocolate;
-                this.consume_chocolate(this.pos.x,this.pos.y);
-                return true;
-            } else if (type == "T") {
-                this.reward = -2;
-                return true;
-            } else {
-                this.reward = 0;
-                return true
-            }
+        } else if (type == "C"){
+            this.reward = this.chocolate;
+            this.pos.x = 0; this.pos.y = 0;
+            return false;
         }
+        this.reward = 0;
+        return true
     }
 }
 
