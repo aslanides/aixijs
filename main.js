a39a74d2b00751be1f5fd81c345e4bb2edaff5f9
John Stewart Aslanides <john.aslanides@anu.edu.au>
2016-04-11 18:08:37 +1000

    fix options in simulate(...) call

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function start() {
     env = new SimpleEpisodicGrid(episodic1)
     options = new Options(env)
     agent = new QLearn(options)
-    history = simulate(env,agent,t_max)
+    history = simulate(env,agent,options.t_max)
     vis = new Visualisation(env,history)
 }
