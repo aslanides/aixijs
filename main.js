502fbdd4d9891d93210c4295fa7bc2651a5203c3
John Stewart Aslanides <john.aslanides@anu.edu.au>
2016-04-13 11:46:58 +1000

    un-break main.js. let's fix this with user-specified setup in main.html

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function start() {
-    env = new SimpleDispenserGrid(dispenser1)
+    //env = new SimpleDispenserGrid(dispenser1)
+    env = new SimpleEpisodicGrid(episodic1)
     options = new Options(env)
-    options.set_model_class(dispenser1)
-    options.set_prior("Mu",env.grid.disp[0][0],env.grid.disp[0][1])
-    agent = new BayesAgent(options)
-    history = simulate(env,agent,options.t_max)
-    vis = new Visualisation(env,history)
+    //options.set_model_class(dispenser1)
+    //options.set_prior("Mu",env.grid.disp[0][0],env.grid.disp[0][1])
+    //agent = new BayesAgent(options)
+    agent = new QLearn(options)
+    hist = simulate(env,agent,options.t_max)
+    vis = new Visualisation(env,hist)
 }
