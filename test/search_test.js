55a38ed21aaec3205909e638ff699856dfecc468
John Stewart Aslanides <john.aslanides@anu.edu.au>
2016-07-06 17:20:12 +1000

    fix camel case for thompson sampling

--- test/search_test.js
+++ test/search_test.js
@@ -1,5 +1,5 @@
 QUnit.test('Search', function (assert) {
-	let mock_agent = {
+	let mockAgent = {
 		horizon: 3,
 		UCBweight: 1,
 		max_reward: config.rewards.chocolate + config.rewards.move,
@@ -15,14 +15,14 @@ QUnit.test('Search', function (assert) {
 		mu: 1,
 	});
 	model.save();
-	let tree = new ExpectimaxTree(mock_agent, model);
+	let tree = new ExpectimaxTree(mockAgent, model);
 	for (let i = 0; i < 10; i++) {
 		assert.equal(tree.bestAction(), 3);
 	}
 });
 
 QUnit.test('Search', function (assert) {
-	let mock_agent = {
+	let mockAgent = {
 		horizon: 5,
 		UCBweight: 1,
 		max_reward: config.rewards.chocolate + config.rewards.move,
@@ -37,19 +37,19 @@ QUnit.test('Search', function (assert) {
 		priorType: 'Informed',
 		mu: 1,
 	});
-	let tree = new ExpectimaxTree(mock_agent, model);
+	let tree = new ExpectimaxTree(mockAgent, model);
 	for (let i = 0; i < 10; i++) {
 		assert.equal(tree.bestAction(), 3);
 	}
 
 	let rho = model.modelClass[5];
-	let tree2 = new ExpectimaxTree(mock_agent, rho);
+	let tree2 = new ExpectimaxTree(mockAgent, rho);
 	for (let i = 0; i < 10; i++) {
 		assert.equal(tree2.bestAction(), 1);
 	}
 
 	let rho2 = model.modelClass[10];
-	let tree3 = new ExpectimaxTree(mock_agent, rho2);
+	let tree3 = new ExpectimaxTree(mockAgent, rho2);
 	for (let i = 0; i < 10; i++) {
 		assert.equal(tree3.bestAction(), 1);
 	}
