64c155e54344c9b933ec09e6eb3d0f7876586694
John Stewart Aslanides <john.aslanides@anu.edu.au>
2016-07-21 21:58:15 +1000

    add Model class as environment wrapper

--- test/model_test.js
+++ test/model_test.js
@@ -1,5 +1,12 @@
 QUnit.test('GridModel', function (assert) {
-	assert.ok(true);
-	cfg = config.environments.dispenser2;
-	model = new Model(SimpleDispenserGrid, cfg);
+	let cfg = config.environments.dispenser2;
+	let model = new Model(cfg);
+	model.perform(1);
+	assert.equal(model.env.pos, model.env.grid[1][0]);
+	for (let i = 0; i < 500; i++) {
+		let e = model.generatePercept();
+		model.perform(4);
+		let p = model.conditionalDistribution(e);
+	}
+
 });
