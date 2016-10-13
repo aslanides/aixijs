a59d63dd6a0764d5e80c1f5d66da26468a6f0900
John Aslanides <john.stewart.aslanides@gmail.com>
2016-10-13 12:05:33 +1100

    [working] version with straight links

--- src/vis/mdp2vis.js
+++ src/vis/mdp2vis.js
@@ -35,89 +35,56 @@ class MDP2Vis extends Visualization {
 			.attr('height', this.height + 2 * this.margin);
 		let nodes = graph.nodes;
 		let nodeById = d3.map(nodes, d => d.id);
-		let edges = graph.edges;
+		let links = graph.edges;
 		let bilinks = [];
 		let svg = this.svg;
 
-		svg.append('defs').selectAll('marker')
-		.data(['end'])
-		.enter().append('marker')
-		.attr('id', 'arrowhead')
-		.attr('viewBox', '0 -5 10 10')
-		.attr('refX', 15)
-		.attr('refY', -1.5)
-		.attr('markerWidth', 6)
-		.attr('markerHeight', 6)
-		.attr('orient', 'auto')
-		.append('path')
-		.attr('d', 'M0,-5L10,0L0,5');
-
-		edges.forEach(edge => {
-			let s = edge.source = nodeById.get(edge.source);
-			let t = edge.target = nodeById.get(edge.target);
-			let i = { a: edge.a }; // intermediate node
-			nodes.push(i);
-
-			if (s == t) {
-				let j = { a: edge.a };
-				nodes.push(j);
-				edges.push({ source: s, target: i }, { source: i, target: j }, { source: j, target: t });
-				bilinks.push([s, i, j, s]);
-				return;
-			}
-
-			edges.push({ source: s, target: i }, { source: i, target: t });
-			bilinks.push([s, i, t]);
-		});
-
-		let link = svg.selectAll('.link')
-			.data(bilinks)
-			.enter().append('path')
-			.attr('fill', 'none')
-			.style('stroke', d => color(d[1].a))
-			.attr('marker-end', _ => `url(#arrowhead)`);
-
-		let node = svg.selectAll('.node')
-			.data(nodes.filter(d => d.id != undefined))
-			.enter().append('circle')
-			.attr('class', 'node')
-			.attr('r', 30)
-			.attr('fill', d => color(d.group))
-			.call(d3.drag()
-			.on('start', dragstarted)
-			.on('drag', dragged)
-			.on('end', dragended));
-
-		node.append('title')
-			.text(d => d.id);
-
 		let simulation = d3.forceSimulation()
+		.force('link', d3.forceLink().distance(100).strength(0.1))
 			.force('center', d3.forceCenter(width / 2, height / 2))
 			.force('charge', d3.forceManyBody().strength(-50));
 
-		simulation.nodes(nodes)
-			.on('tick', ticked);
+		var link = svg.append('g')
+							.attr('class', 'link')
+					.selectAll('line')
+					.data(links)
+					.enter().append('line')
+					.style('stroke', d => color(d.a))
+					.attr('stroke-width', 5);
+
+		var node = svg.append('g')
+						.attr('class', 'nodes')
+				.selectAll('circle')
+				.data(graph.nodes)
+				.enter().append('circle')
+						.attr('r', 30)
+						.attr('fill', function (d) { return color(d.group); })
+						.call(d3.drag()
+										.on('start', dragstarted)
+										.on('drag', dragged)
+										.on('end', dragended));
+
+		node.append('title')
+						.text(function (d) { return d.id; });
+
+		simulation
+						.nodes(graph.nodes)
+						.on('tick', ticked);
+
+		simulation.force('link')
+						.links(links);
 
 		function ticked() {
-			link.attr('d', positionLink);
-			node.attr('transform', positionNode);
+			link
+							.attr('x1', function (d) { return d.source.x; })
+							.attr('y1', function (d) { return d.source.y; })
+							.attr('x2', function (d) { return d.target.x; })
+							.attr('y2', function (d) { return d.target.y; });
+
+			node
+							.attr('cx', function (d) { return d.x; })
+							.attr('cy', function (d) { return d.y; });
 		}
-
-		function positionLink(d) {
-			let curve = 'S';
-			if (d.length == 4) {
-				curve = 'C';
-			}
-
-			let s = `M${d[0].x},${d[0].y}${curve}${d[1].x},${d[1].y} ${d[2].x},${d[2].y}`;
-			if (d.length == 4) {
-				s += ` ${d[3].x},${d[3].y}`;
-			}
-
-			return s;
-		}
-
-		let positionNode = d => `translate(${d.x},${d.y})`;
 
 		function dragstarted(d) {
 			if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -137,7 +104,7 @@ class MDP2Vis extends Visualization {
 		}
 
 		this.nodes = nodes;
-		this.edges = edges;
+		this.edges = links;
 
 	}
 
